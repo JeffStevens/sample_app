ab777098b6d07bc645ddccc03f8d9d6700446d34
QuangVinh <vinhgosu@gmail.com>
2016-03-16 08:25:15 +0700

    Account activation password reset

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "quangvinh181193@gmail.com"
+  layout 'mailer'
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,22 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
+  config.action_mailer.default_url_options = {host: "localhost", port: 3000}
+
+  config.action_mailer.default_url_options = {host: "myapp.herokuapp.com"}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default charset: "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "myapp.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: 'quangvinh181193@gmail.com',
+    password: 'xeko1811'
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
